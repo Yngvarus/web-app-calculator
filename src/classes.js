fe70e275be43e17c3af33fb482c0c5832b2d9f26
Yngvarus <103531512+Yngvarus@users.noreply.github.com>
2024-05-19 15:28:12 +0200

    token class coding and minor changes

--- src/classes.js
+++ src/classes.js
@@ -11,15 +11,11 @@ class Token {
     static TokenAssociativity = Object.freeze({
         LEFT: Symbol("left"),
         RIGHT: Symbol("right"),
+        UNARY: Symbol("unary")
     });
 
-    constructor() {
-        this.tokenType = Token.TokenType.NOT;
-        this.value = 0.0;
-        this.symbol = " ";
-        this.assoc = Token.TokenAssociativity.RIGHT;
-        this.precedence = 0;
-        this.parameterCount = 0;
+    constructor(type) {
+        this.tokenType = type;
     }
 
     static StringToToken(str) {
@@ -29,7 +25,86 @@ class Token {
             //else
         }
     }
+}
 
+class numToken extends Token {
+    constructor(value){
+        super(Token.TokenType.NUM);
+        this.value = value;
+    }
+
+}
+
+class OptToken extends Token {
+    static bracket = Object.freeze(["(", ")", "[", "]"]);
+    
+    constructor(symbol) {
+
+        if (OptToken.bracket.includes(symbol)) {
+            for(let i=0; i < OptToken.bracket.length; i++) {
+                if(i%2==0){
+                    if(OptToken.bracket[i] === symbol){
+                        super(Token.TokenType.LBR);
+                    }
+
+                } else{
+                    if(OptToken.bracket[i] === symbol){
+                        super(Token.TokenType.RBR);
+                    }
+                }
+            }
+            this.symbol = symbol;
+        } else {
+            super(Token.TokenType.OPT);
+
+            this.symbol = symbol;
+        
+            switch(symbol){
+                case '±':
+                    this.assoc = Token.TokenAssociativity.UNARY;
+                    this.precedence = 14;
+                    this.parameterCount = 1;
+                    break;
+                case '÷':
+                    this.assoc = Token.TokenAssociativity.LEFT;
+                    this.precedence = 12;
+                    this.parameterCount = 2;
+                    break;
+                case '×':
+                    this.assoc = Token.TokenAssociativity.LEFT;
+                    this.precedence = 12;
+                    this.parameterCount = 2;
+                    break;
+                case '-':
+                    this.assoc = Token.TokenAssociativity.LEFT;
+                    this.precedence = 11;
+                    this.parameterCount = 2;
+                    break;
+                case '+':
+                    this.assoc = Token.TokenAssociativity.LEFT;
+                    this.precedence = 11;
+                    this.parameterCount = 2;
+                    break;
+                case '%':
+                    break;
+                case '√(x)':
+                    this.assoc = Token.TokenAssociativity.UNARY;
+                    this.precedence = 13;
+                    this.parameterCount = 1;
+                    break;
+                case '(x)²':
+                    this.assoc = Token.TokenAssociativity.UNARY;
+                    this.precedence = 13;
+                    this.parameterCount = 1;
+                    break;
+                case '(':
+                    break;
+                case ')':
+                    break;
+            }
+        }
+
+    }
 }
 
 class Calculator {
